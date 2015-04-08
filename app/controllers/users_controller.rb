2f10c4860a024c1c37412638d56b25f8ef1f9b07
Dalal Alnasrallah <dalalalnasrallah@hotmail.com>
2015-04-08 11:56:50 +0100

    Admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,16 +23,18 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
+ def create
     @user = User.new(user_params)
+    role = Role.where(:title => "user").first
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+        @user.role = role if role.present?
+        format.html { redirect_to @user, :notice => 'User was successfully created.' }
+        format.json { render :show, :status => :created, :location => @user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render :json => @user.errors, :status => :unprocessable_entity }
       end
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+  has_one :role, :through => :role_user
+  has_one :role_user
+
+  def admin?
+    self.role.title == "admin"
+  end
+
+  def user?
+    self.role.title == "user"
+  end
 end
